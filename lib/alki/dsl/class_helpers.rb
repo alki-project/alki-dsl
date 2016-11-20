be450115780a40c00834c9117065541be8c61db2
Matt Edlefsen <matt.edlefsen@gmail.com>
2016-11-20 12:40:54 -0800

    Add support for working with subclasses in class_helpers

--- lib/alki/dsl/class_helpers.rb
+++ lib/alki/dsl/class_helpers.rb
@@ -1,53 +1,65 @@
 module Alki
   module Dsl
     module ClassHelpers
-      def class_builder
+      def class_builder(subclass = nil)
         unless @ctx[:class_builder]
           @ctx[:class_builder] = {}
           %i(name prefix).each do |attr|
             @ctx[:class_builder][attr] = @ctx[attr] if @ctx[attr]
           end
         end
-        @ctx[:class_builder]
+        if subclass
+          scs = @ctx[:class_builder][:secondary_classes] ||= []
+          cb = scs.find { |sc| sc[:subclass] == subclass }
+          unless cb
+            cb = { subclass: subclass }
+            scs << cb
+          end
+          cb
+        else
+          @ctx[:class_builder]
+        end
       end
 
-      def create_as_module
-        class_builder[:type] = :module
+      def create_as_module(subclass: nil)
+        class_builder(subclass)[:type] = :module
       end
 
-      def set_super_class(klass)
-        class_builder[:super_class] = klass
+      def set_super_class(klass,subclass: nil)
+        class_builder(subclass)[:super_class] = klass
       end
 
-      def add_method(name,private: false, &blk)
-        class_builder[:instance_methods] ||= {}
-        class_builder[:instance_methods][name.to_sym] = {
+      def add_method(name,context:nil,private: false,subclass: nil, &blk)
+        class_builder(subclass)[:instance_methods] ||= {}
+        class_builder(subclass)[:instance_methods][name.to_sym] = {
           body: blk,
+          context: context,
           private: private
         }
       end
 
-      def add_class_method (name,private: false,&blk)
-        class_builder[:class_methods] ||= {}
-        class_builder[:class_methods][name.to_sym] = {
+      def add_class_method (name,context: nil,private: false,subclass: nil,&blk)
+        class_builder(subclass)[:class_methods] ||= {}
+        class_builder(subclass)[:class_methods][name.to_sym] = {
           body: blk,
+          context: context,
           private: private
         }
       end
 
-      def add_initialize_param(name)
-        class_builder[:initialize_params] ||= []
-        class_builder[:initialize_params] << name.to_sym
+      def add_initialize_param(name,subclass: nil)
+        class_builder(subclass)[:initialize_params] ||= []
+        class_builder(subclass)[:initialize_params] << name.to_sym
       end
 
-      def add_instance_class_proxy(type, name)
-        class_builder[:instance_class] ||= {}
-        class_builder[:instance_class][name.to_sym] = {type: type}
+      def add_instance_class_proxy(type, name,subclass: nil)
+        class_builder(subclass)[:instance_class] ||= {}
+        class_builder(subclass)[:instance_class][name.to_sym] = {type: type}
       end
 
-      def add_module(mod)
-        class_builder[:modules] ||= []
-        class_builder[:modules] << mod
+      def add_module(mod,subclass: nil)
+        class_builder(subclass)[:modules] ||= []
+        class_builder(subclass)[:modules] << mod
       end
     end
   end
