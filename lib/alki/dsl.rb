3267d3d55ff46bb756c16ec426d03673c4d51c80
Matt Edlefsen <matt.edlefsen@gmail.com>
2017-01-16 18:41:05 -0800

    Add Alki::Dsl::merge to allow quick merging of dsls

--- lib/alki/dsl.rb
+++ lib/alki/dsl.rb
@@ -1,7 +1,16 @@
 require 'alki/loader'
+require 'alki/dsls/dsl'
 
 module Alki
   module Dsl
+    def self.merge(*dsls)
+      Alki::Dsls::Dsl.build do
+        dsls.each do |dsl|
+          require_dsl dsl
+        end
+      end
+    end
+
     def self.build(name,data={},&blk)
       Alki.load(name).build data, &blk
     end
